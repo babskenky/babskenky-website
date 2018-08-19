93a04d33de651738ccb39d57118865b2cb0161b2
michaeleze <michaeleze3@gmail.com>
2018-08-19 08:49:00 +0100

    update

--- babskeny/src/App.js
+++ babskeny/src/App.js
@@ -15,8 +15,6 @@ class App extends Component {
                       <meta charSet="utf-8"/>
                           <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
                               <meta name="theme-color" content="#000000"/>
-
-                                  <!-- SEO -->
                                   <meta name="author" content="Michael Eze"/>
                                   <meta name="description" content="Baba Nureni-Yusuf - Author |Co-Founder |Internet Entrepreneur |Product Strategy Expert | Global Trainer | Public Speaker | Internet Marketer | Coach | Digital Economy Evangelist
 "/>
