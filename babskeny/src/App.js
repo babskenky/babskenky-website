9caf7a85f8fe9ae79ed928ba160323822ea19da2
michaeleze <michaeleze3@gmail.com>
2018-08-20 12:33:26 +0100

    analytics added

--- babskeny/src/App.js
+++ babskeny/src/App.js
@@ -30,7 +30,6 @@ class App extends Component {
                                         content="Baba Nureni-Yusuf - Author |Co-Founder |Internet Entrepreneur |Product Strategy Expert | Global Trainer | Public Speaker | Internet Marketer | Coach | Digital Economy Evangelist"/>
                                   <meta name="viewport"
                                         content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
-                  <!-- Google Analytics -->
                   <script>
                       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
                       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -40,7 +39,6 @@ class App extends Component {
                       ga('create', 'UA-XXXXX-Y', 'auto');
                       ga('send', 'pageview');
                   </script>
-                  <!-- End Google Analytics -->
                                                                               <title>Babskenky</title>
               </Helmet>
           <Nav/>
