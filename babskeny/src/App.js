944e59c2e31222101608e2610a6f69317a91fad0
michaeleze <michaeleze3@gmail.com>
2018-08-20 12:35:18 +0100

    removed analytics

--- babskeny/src/App.js
+++ babskeny/src/App.js
@@ -30,15 +30,6 @@ class App extends Component {
                                         content="Baba Nureni-Yusuf - Author |Co-Founder |Internet Entrepreneur |Product Strategy Expert | Global Trainer | Public Speaker | Internet Marketer | Coach | Digital Economy Evangelist"/>
                                   <meta name="viewport"
                                         content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
-                  <script>
-                      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-                  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-                      ga('create', 'UA-XXXXX-Y', 'auto');
-                      ga('send', 'pageview');
-                  </script>
                                                                               <title>Babskenky</title>
               </Helmet>
           <Nav/>
